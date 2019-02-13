77eeb7ee91c07d062a8c6fe025836765b9cc9538
AMarchmarti <marchmarti.antoni@gmail.com>
2019-02-13 17:36:24 +0100

    Creado un nuevo pryoceto Maven para el examen PigCoin

---
+++ src/main/java/org/lasencinas/App.java
@@ -0,0 +1,29 @@
+package org.mvpigs.pigcoin;
+
+import java.security.KeyPair;
+import java.util.Map;
+
+public class App {
+
+    public static void main( String[] args )
+    {
+        /**
+         * Crea una wallet
+         * Genera las claves privada y publica de la wallet
+         */
+
+        System.out.println("\n" + "Ver clave Privada y clave Pública de una wallet" + "\n" +
+                "==============================================="        );
+
+        Wallet wallet_1 = new Wallet();
+        KeyPair pair = GenSig.generateKeyPair();
+        wallet_1.setSK(pair.getPrivate());
+        wallet_1.setAddress(pair.getPublic());
+
+        System.out.println("\n Direccion de la Wallet_1: \n" +  wallet_1.getAddress().hashCode());}}
+
+        /**
+         * Crea una segunda wallet, esta vez generando sus claves
+         * con un metodo wallet.generateKeyPair() que encapsula
+         * el codigo de la anterior historia de usuario
+         */
