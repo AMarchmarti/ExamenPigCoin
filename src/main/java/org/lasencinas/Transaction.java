dd6d078340168c898e1e2edaf87bb96dcd09d41d
AMarchmarti <marchmarti.antoni@gmail.com>
2019-02-13 19:51:36 +0100

    Transaction termianda

--- src/main/java/org/lasencinas/Transaction.java
+++ src/main/java/org/lasencinas/Transaction.java
@@ -9,7 +9,7 @@ public class Transaction {
         private String prev_hash = null;
         private PublicKey pKey_sender = null;
         private PublicKey pKey_recipient = null;
-        private double pigcoins = 0;
+        private double pigcoins = 0d;
         private String message = null;
         private byte [] signature = null;
 
