baff65a7757f7ef97d008a6bb517f0280a65f74d
AMarchmarti <marchmarti.antoni@gmail.com>
2019-02-13 18:59:15 +0100

    Aplicados casos test

--- src/test/java/org/lasencinas/WalletTest.java
+++ src/test/java/org/lasencinas/WalletTest.java
@@ -3,11 +3,23 @@ package org.lasencinas;
 import org.junit.Assert;
 import org.junit.Test;
 
+import static org.junit.Assert.assertNotNull;
+
 public class WalletTest {
 
     @Test
-    public void WalleTest(){
-        Assert.assertNotNull(null, new Wallet());
+    public void constructor_test() {
+        Wallet wallet = new Wallet();
+        assertNotNull(wallet);
+    }
+
+    @Test
+    public void generate_key_pair_test() {
+        Wallet wallet = new Wallet();
+        assertNotNull(wallet);
+        wallet.generateKeyPair();
+        assertNotNull(wallet.getAddress());
+        assertNotNull(wallet.getSKey());
     }
 
 }
