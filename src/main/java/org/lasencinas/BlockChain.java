64aaeff7d4467561d871cc4517183c722e4f469f
AMarchmarti <marchmarti.antoni@gmail.com>
2019-02-13 19:50:41 +0100

    Añadida la funcion addOrigin

--- src/main/java/org/lasencinas/BlockChain.java
+++ src/main/java/org/lasencinas/BlockChain.java
@@ -1,4 +1,14 @@
 package org.lasencinas;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class BlockChain {
+
+        private List<Transaction> blockChain = new LinkedList<>();
+
+
+        public void addOrigin(Transaction tran){
+            blockChain.add(tran);
+        }
 }
