24c6304516b316955218d95b4aef05b1d3ff6edf
AMarchmarti <44237549+AMarchmarti@users.noreply.github.com>
2019-02-17 14:03:22 +0100

    Añadida la inyeccion de Dependencia de BlockChain a Wallet

--- src/main/java/org/lasencinas/Wallet.java
+++ src/main/java/org/lasencinas/Wallet.java
@@ -3,6 +3,8 @@ package org.lasencinas;
 import java.security.KeyPair;
 import java.security.PrivateKey;
 import java.security.PublicKey;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Wallet {
 
@@ -11,8 +13,8 @@ public class Wallet {
         private double total_input = 0;
         private double total_output = 0;
         private double balance = 0;
-        private int inputTransactions = 0;
-        private int outputTransactins = 0;
+        private List<Transaction> inputTransactions = new LinkedList<>();
+        private List<Transaction> outputTransactions = new LinkedList<>();
 
     /*-------------Setters-------------------------*/
 
@@ -36,6 +38,15 @@ public class Wallet {
             this.balance = getTotalInput() - getTotalOutput();
         }
 
+        public void setInputTransactions(List<Transaction> lista){
+            this.inputTransactions = lista;
+        }
+
+
+        public void setOutputTransactions(List<Transaction> lista){
+            this.outputTransactions = lista;
+        }
+
     /*----------------Getters-----------------------*/
 
         public PublicKey getAddress(){
@@ -55,6 +66,17 @@ public class Wallet {
 
 
         public double getBalance(){return this.balance;}
+
+
+        public List<Transaction> getInputTransactions(){
+            return this.inputTransactions;
+        }
+
+
+        public List<Transaction> getOutputTransactions(){
+            return this.outputTransactions;
+        }
+
 
     /*----------------Lógica------------------------*/
 
@@ -77,5 +99,15 @@ public class Wallet {
             setTotal_output(transferencia[1]);
             setBalance();
         }
+
+
+
+        public void loadInputTransactions(BlockChain bChain){
+            setInputTransactions(bChain.loadInputTransactions(getAddress()));
         }
+
+        public void loadOutputTransactions(BlockChain bChain){
+            setOutputTransactions(bChain.loadOutputTransactions(getAddress()));
+        }
+}
 
