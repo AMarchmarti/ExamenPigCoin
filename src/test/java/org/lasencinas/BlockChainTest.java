20eddabd7e9bd87fae7e0a265ec310142ca777b1
AMarchmarti <44237549+AMarchmarti@users.noreply.github.com>
2019-02-17 19:11:28 +0100

    Nuevos casos test para las siguientes funciones, algunos comentados para su posterior uso

--- src/test/java/org/lasencinas/BlockChainTest.java
+++ src/test/java/org/lasencinas/BlockChainTest.java
@@ -3,7 +3,9 @@ package org.lasencinas;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 import static junit.framework.TestCase.assertNotNull;
 import static junit.framework.TestCase.assertTrue;
@@ -97,5 +99,63 @@ public class BlockChainTest {
         assertTrue(inputTransactions.size() == 0);
         assertFalse(inputTransactions.contains(transaction));
     }
+
+    @Test
+    public void is_consumed_coin_valid_test() {
+
+        Wallet origin = new Wallet();
+        origin.generateKeyPair();
+        Wallet wallet_1 = new Wallet();
+        wallet_1.generateKeyPair();
+        Wallet wallet_2 = new Wallet();
+        wallet_2.generateKeyPair();
+
+        BlockChain bChain = new BlockChain();
+        Transaction transaction = new Transaction("hash_1", "0", origin.getAddress(), wallet_1.getAddress(), 20, "spam eggs");
+        bChain.addOrigin(transaction);
+        transaction = new Transaction("hash_2", "1", origin.getAddress(), wallet_2.getAddress(), 10, "spam spam spam");
+        bChain.addOrigin(transaction);
+        transaction = new Transaction("hash_3", "hash_1", wallet_1.getAddress(), wallet_2.getAddress(), 10, "pig things!");
+        bChain.addOrigin(transaction);
+
+        Map<String, Double> consumedCoins = new LinkedHashMap<>();
+        consumedCoins.put("hash_1", 10d);
+        assertFalse(bChain.isConsumedCoinValid(consumedCoins));
+        consumedCoins.clear();
+        consumedCoins.put("hash_2", 10d);
+        consumedCoins.put("hash_3", 10d);
+        assertTrue(bChain.isConsumedCoinValid(consumedCoins));
+    }
+/*
+    @Test
+    public void create_transaction_test() {
+
+        Wallet origin = new Wallet();
+        origin.generateKeyPair();
+        Wallet wallet_1 = new Wallet();
+        wallet_1.generateKeyPair();
+        Wallet wallet_2 = new Wallet();
+        wallet_2.generateKeyPair();
+
+        BlockChain bChain = new BlockChain();
+        Transaction transaction = new Transaction("hash_1", "0", origin.getAddress(), wallet_1.getAddress(), 20, "a flying pig!");
+        bChain.addOrigin(transaction);
+        transaction = new Transaction("hash_2", "1", origin.getAddress(), wallet_2.getAddress(), 10, "pig things!");
+        bChain.addOrigin(transaction);
+
+        Map<String, Double> consumedCoins = new LinkedHashMap<>();
+        consumedCoins.put("hash_1", 10.2d);
+        consumedCoins.put("CA_hash_2", 9.8d);
+        assertTrue(bChain.isConsumedCoinValid(consumedCoins));
+
+
+        int previousBlockChainSize = bChain.getBlockChain().size();
+        bChain.createTransaction(wallet_1.getAddress(), wallet_2.getAddress(), consumedCoins, "pig things!", wallet_1.signTransaction("pig things!"));
+        assertEquals(previousBlockChainSize + consumedCoins.size(), bChain.getBlockChain().size(), 0);
+        assertEquals("hash_4", bChain.getBlockChain().get(3).getHash());
+        assertEquals(9.8, bChain.getBlockChain().get(3).getPigCoins(), 0);
+        bChain.summarize(3);
+    }
+*/
 
 }
