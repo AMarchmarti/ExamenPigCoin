4e2b98d1eb0d04959e5b8b4fe4a83a6eefa1c6ee
AMarchmarti <44237549+AMarchmarti@users.noreply.github.com>
2019-02-17 11:29:01 +0100

    Aplicado nuevo caso test, cargar el monedero

--- src/test/java/org/lasencinas/BlockChainTest.java
+++ src/test/java/org/lasencinas/BlockChainTest.java
@@ -3,6 +3,8 @@ package org.lasencinas;
 import org.junit.Assert;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class BlockChainTest {
 
     @Test
@@ -30,4 +32,30 @@ public class BlockChainTest {
         bChain.addOrigin(trx);
         Assert.assertNotNull(bChain);
     }
+
+    @Test
+    public void load_wallet_test() {
+
+        Wallet origin = new Wallet();
+        origin.generateKeyPair();
+        Wallet wallet_1 = new Wallet();
+        wallet_1.generateKeyPair();
+        Wallet wallet_2 = new Wallet();
+        wallet_2.generateKeyPair();
+
+        BlockChain bChain = new BlockChain();
+        Transaction transaction = new Transaction("hash_1", "0", origin.getAddress(), wallet_1.getAddress(), 20, "spam eggs");
+        bChain.addOrigin(transaction);
+        transaction = new Transaction("hash_2", "1", origin.getAddress(), wallet_2.getAddress(), 10, "spam eggs spam");
+        bChain.addOrigin(transaction);
+        transaction = new Transaction("hash_3", "hash_1", wallet_1.getAddress(), wallet_2.getAddress(), 10, "pig things!");
+        bChain.addOrigin(transaction);
+        transaction = new Transaction("hash_4", "CA_hash_3", wallet_1.getAddress(), wallet_1.getAddress(), 10, "spam spam spam");
+        bChain.addOrigin(transaction);
+
+        double[] pigcoins = bChain.loadWallet(wallet_1.getAddress());
+        assertEquals(30, pigcoins[0], 0);
+        assertEquals(20, pigcoins[1], 0);
+    }
+
 }
